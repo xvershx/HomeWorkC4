dc494d590a84677ae3ab40e88885476c167176ec
xvershx <alexey.versh2000@gmail.com>
2024-02-24 18:04:13 +0300

    two

--- oneC.c
+++ oneC.c
@@ -2,7 +2,7 @@
 main()
 {
  int a, b, c, raz; 
- printf("Input number:\n");
+ printf("Input srdgdgnumber:\n");
  scanf ("%d%d", &a, &b); 
  raz = a - b; 
  printf("%d %s %d %s %d", a, "-" , b ,"=" , raz);
